f9a6b5dc781711ea40313c20c8002cb9f4c06039
Blatantbeast <35450175+Blatantbeast@users.noreply.github.com>
2018-10-19 21:35:23 +0200

    MovePlayer

---
+++ RageQuit/Assets/MovePlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+
+    public GameObject PlateformeMouvante;
+    public GameObject Player;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.parent = PlateformeMouvante.transform;
+
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.parent = null;
+
+        }
+    }
+}
