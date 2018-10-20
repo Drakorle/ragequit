7f9001987d52a98983a5824cb57982bde390fae3
Drakorle <blackdeal@live.fr>
2018-10-20 20:30:54 +0200

    Maj Camera
    - le detecteur de collision ne prend plus en compte la hitbox du joueur
    - ajout du zoom
    - ajout d'un Smooth pour ramener la camera apres un alt (alt = regarder autour du personnage)
    - ajout firstPerson (touche V) & réorganisation de la hiérarchie
    - ajout de parametres & suppression des var / params inutiles + légères optimisations avec le zoom

--- RageQuit/Assets/Personnages/Scripts/Camera/CameraFollower.cs
+++ RageQuit/Assets/Personnages/Scripts/Camera/CameraFollower.cs
@@ -4,14 +4,18 @@ public class CameraFollower : MonoBehaviour
 {
 	public GameObject FollowedPlayer;
 	public float MaxClampAngle = 80.0f;
-	public float MinClampAngle = -80.0f;
+	public float MinClampAngle = -60.0f;
 	public float CameraVerticalSensitivity = 150.0f;
     public float CameraHorizontalSensitivity = 150.0f;
     public float CameraAltRecoverySpeed = 0.2f;
+    public float RotationRecoveringSmooth = 10.0f; //Smooth the speed of camera recovery after alt push
+    public KeyCode FreeCameraKey = KeyCode.LeftAlt;
+
     private float rotationX;
     private float lastRotationX;
 	private float rotationY;
     private bool altPreviouslyPushed;
+    private bool rotationRecovering;
 	
 	// Use this for initialization
 	void Start ()
@@ -22,17 +26,19 @@ public class CameraFollower : MonoBehaviour
         altPreviouslyPushed = false;
         Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;
+        rotationRecovering = false; // set back the rotation behind the player after an alt push
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-        bool altPushed = Input.GetKey(KeyCode.LeftAlt);
+        bool altPushed = Input.GetKey(FreeCameraKey);
 
         if (altPreviouslyPushed && !altPushed)
         {
             rotationX = lastRotationX;
             rotationY = FollowedPlayer.transform.localRotation.eulerAngles.y;
+            rotationRecovering = true;
         }
 
         //mouse & stick inputs supported
@@ -43,7 +49,6 @@ public class CameraFollower : MonoBehaviour
 		rotationY += (FinalInputX * CameraHorizontalSensitivity) * Time.deltaTime;
 		rotationX += (FinalInputZ * CameraVerticalSensitivity) * Time.deltaTime;
 
-
 		rotationX = Mathf.Clamp(rotationX, MinClampAngle, MaxClampAngle);
 
         if (!altPushed)
@@ -52,7 +57,14 @@ public class CameraFollower : MonoBehaviour
             FollowedPlayer.transform.rotation = Quaternion.Euler(0, rotationY, 0); //player orientation
         }
 
-        transform.rotation = Quaternion.Euler(rotationX, rotationY, 0); //camera orientation
+        if (rotationRecovering)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(rotationX, rotationY, 0), Time.deltaTime * RotationRecoveringSmooth); //camera orientation
+            if (transform.rotation == Quaternion.Euler(rotationX, rotationY, 0))
+                rotationRecovering = false;
+        }
+        else
+            transform.rotation = Quaternion.Euler(rotationX, rotationY, 0);
 
         altPreviouslyPushed = altPushed;
     }
---
+++ RageQuit/Assets/Personnages/Scripts/Camera/ThirdPersonView.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonView : MonoBehaviour
+{
+    public float MinDistance = 1.0f;
+    public float MaxDistance = 6.0f;
+    public float CurrentMaxDistance = 4.0f; //default distance at start (if no collision), and current distance of the player during the game
+    public float ZoomModifier = 1.0f;
+    public float CollisionZoomSmooth = 10.0f; //smooth for camera move when collision is detected
+    public float HitDistanceCorrector = 0.85f;
+    public LayerMask LayerToIgnore;
+    public GameObject PlayerView;
+    public Collider ObjectDetector;
+    public Vector2 ObjectDetectorSize = new Vector2(0.2f, 0.2f);
+    public float ObjectDetectorShift = 0.5f; //distance of objectDetector from player to avoid useless collision with his hitbox (even if they'rent considered)
+
+    private int notIgnoredLayers;
+    private Vector3 dollyDir;
+    private Dictionary<int, Transform> colliderTransforms;
+
+    void OnEnable()
+    {
+        CurrentMaxDistance = Mathf.Clamp(CurrentMaxDistance, MinDistance, MaxDistance);
+        dollyDir = new Vector3(0,0,-1);
+        notIgnoredLayers = int.MaxValue - LayerToIgnore.value;
+        colliderTransforms = new Dictionary<int, Transform>();
+        ObjectDetector.transform.localPosition = new Vector3(ObjectDetector.transform.localPosition.x, ObjectDetector.transform.localPosition.y, CurrentMaxDistance * dollyDir.z / 2 - ObjectDetectorShift);
+        ObjectDetector.transform.localScale = new Vector3(ObjectDetectorSize.x, ObjectDetectorSize.y, CurrentMaxDistance - ObjectDetectorShift);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Zoom/Dezoom
+        float zoomChange = Input.GetAxis("Mouse ScrollWheel");
+
+        if (zoomChange != 0)
+        {
+            CurrentMaxDistance = Mathf.Clamp(CurrentMaxDistance + zoomChange * -ZoomModifier, MinDistance, MaxDistance);
+            ObjectDetector.transform.localPosition = new Vector3(ObjectDetector.transform.localPosition.x, ObjectDetector.transform.localPosition.y, CurrentMaxDistance * dollyDir.z / 2 - ObjectDetectorShift);
+            ObjectDetector.transform.localScale = new Vector3(ObjectDetectorSize.x, ObjectDetectorSize.y, CurrentMaxDistance - ObjectDetectorShift);
+        }
+
+        //Distance ajustment
+        Vector3 desiredCameraPos = transform.parent.transform.TransformPoint(dollyDir * CurrentMaxDistance);
+        RaycastHit hit = new RaycastHit();
+        float distance = CurrentMaxDistance;
+
+        if (Physics.Linecast(transform.parent.transform.position, desiredCameraPos, out hit, notIgnoredLayers))
+        {
+            distance = hit.distance;
+        }
+
+        foreach (KeyValuePair<int, Transform> colliderTransform in colliderTransforms)
+        {
+            Physics.Linecast(transform.parent.transform.position, colliderTransform.Value.position, out hit, notIgnoredLayers);
+            if (hit.distance < distance)
+                distance = hit.distance;
+        }
+
+        distance = Mathf.Clamp(distance * HitDistanceCorrector, MinDistance, MaxDistance);
+
+        PlayerView.transform.localPosition = Vector3.Lerp(PlayerView.transform.localPosition, dollyDir * distance, Time.deltaTime * CollisionZoomSmooth);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player")
+            colliderTransforms.Add(other.GetInstanceID(), other.transform);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag != "Player")
+            colliderTransforms.Remove(other.GetInstanceID());
+    }
+}
