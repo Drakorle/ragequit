4d1cb82ade851c325119fce07f983987cd77256b
Drakorle <blackdeal@live.fr>
2018-12-04 00:24:56 +0100

    Correction bug camera
    + Option: sensibilité mouvement de souris x y

--- RageQuit/Assets/Menus/MainMenu.cs
+++ RageQuit/Assets/Menus/MainMenu.cs
@@ -7,14 +7,29 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     public Slider X_axis_sensitivity;
+    public float Default_X_axis_sensitivity = 1f;
+    public float Min_X_axis_sensitivity = 0.1f;
+    public float Max_X_axis_sensitivity = 2f;
     public Slider Y_axis_sensitivity;
+    public float Default_Y_axis_sensitivity = 1f;
+    public float Min_Y_axis_sensitivity = 0.1f;
+    public float Max_Y_axis_sensitivity = 2f;
 
     public void Start()
     {
         if (!PlayerPrefs.HasKey("X_axis_sensitivity"))
-            PlayerPrefs.SetFloat("X_axis_sensitivity", X_axis_sensitivity.value);
+            PlayerPrefs.SetFloat("X_axis_sensitivity", Default_X_axis_sensitivity);
         if (!PlayerPrefs.HasKey("Y_axis_sensitivity"))
-            PlayerPrefs.SetFloat("Y_axis_sensitivity", Y_axis_sensitivity.value);
+            PlayerPrefs.SetFloat("Y_axis_sensitivity", Default_Y_axis_sensitivity);
+
+        X_axis_sensitivity.maxValue = Max_X_axis_sensitivity;
+        X_axis_sensitivity.minValue = Min_X_axis_sensitivity;
+
+        Y_axis_sensitivity.maxValue = Max_Y_axis_sensitivity;
+        Y_axis_sensitivity.minValue = Min_Y_axis_sensitivity;
+
+        X_axis_sensitivity.value = PlayerPrefs.GetFloat("X_axis_sensitivity");
+        Y_axis_sensitivity.value = PlayerPrefs.GetFloat("Y_axis_sensitivity");
     }
 
     public void LoadScene(string scene)
