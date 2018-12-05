1ad924b8e392e6dac4b8774622d9f3a4ccadcc00
Drakorle <blackdeal@live.fr>
2018-12-05 23:36:32 +0100

    +previous
    correction bugs

--- RageQuit/Assets/Menus/MenuManager.cs
+++ RageQuit/Assets/Menus/MenuManager.cs
@@ -7,6 +7,8 @@ public class MenuManager : MonoBehaviour {
 
     public GameObject MenuHolder;
     public GameObject CameraBase;
+    public GameObject FirstPerson;
+    public GameObject ThirdPerson;
 
     private KeyCode menuAccessKey = KeyCode.Escape;
     private bool menuDisplayed = false;
@@ -14,13 +16,23 @@ public class MenuManager : MonoBehaviour {
 	public void DisplayMenu()
     {
         MenuHolder.SetActive(true);
-        CameraBase.GetComponent<CameraFollower>().IsInMenu(true);
+        CameraBase.GetComponent<CameraFollower>().enabled = false;
+        FirstPerson.GetComponent<FirstPersonView>().enabled = false;
+        ThirdPerson.GetComponent<ThirdPersonView>().enabled = false;
+
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 
     public void HideMenu()
     {
         MenuHolder.SetActive(false);
-        CameraBase.GetComponent<CameraFollower>().IsInMenu(false);
+        CameraBase.GetComponent<CameraFollower>().enabled = true;
+        FirstPerson.GetComponent<FirstPersonView>().enabled = true;
+        ThirdPerson.GetComponent<ThirdPersonView>().enabled = true;
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
--- RageQuit/Assets/Personnages/Scripts/Camera/CameraFollower.cs
+++ RageQuit/Assets/Personnages/Scripts/Camera/CameraFollower.cs
@@ -15,13 +15,10 @@ public class CameraFollower : MonoBehaviour
 	private float rotationY;
     private bool altPreviouslyPushed;
     private bool rotationRecovering;
-    private bool inMenu;
 	
 	// Use this for initialization
 	void Start ()
 	{
-        inMenu = false;
-
         FreeCameraKey = MainMenu.GetKeyCode("FreeViewKey");
 
 		Vector3 rotation = transform.localRotation.eulerAngles;
@@ -36,55 +33,42 @@ public class CameraFollower : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-        if (!inMenu)
+        bool altPushed = Input.GetKey(FreeCameraKey);
+
+        if (altPreviouslyPushed && !altPushed)
         {
-            bool altPushed = Input.GetKey(FreeCameraKey);
-
-            if (altPreviouslyPushed && !altPushed)
-            {
-                rotationX = lastRotationX;
-                rotationY = FollowedPlayer.transform.localRotation.eulerAngles.y;
-                rotationRecovering = true;
-            }
-
-            //mouse & stick inputs supported
-            float FinalInputX = Input.GetAxis("RightStickHorizontal") + Input.GetAxis("Mouse X");
-		    float FinalInputZ = Input.GetAxis("RightStickVertical") + Input.GetAxis("Mouse Y");
-
-		    //charge rotations
-		    rotationY += (FinalInputX * CameraHorizontalSensitivity) * PlayerPrefs.GetFloat("Y_axis_sensitivity") * Time.deltaTime;
-		    rotationX += (FinalInputZ * CameraVerticalSensitivity) * PlayerPrefs.GetFloat("X_axis_sensitivity") * Time.deltaTime;
-
-		    rotationX = Mathf.Clamp(rotationX, MinClampAngle, MaxClampAngle);
-
-            if (!altPushed)
-            {
-                lastRotationX = rotationX;
-                FollowedPlayer.transform.rotation = Quaternion.Euler(0, rotationY, 0); //player orientation
-            }
-
-            Quaternion cameraRotation = Quaternion.Euler(rotationX, rotationY, 0);
-
-            if (rotationRecovering)
-            {
-                transform.rotation = Quaternion.Lerp(transform.rotation, cameraRotation, Time.deltaTime * RotationRecoveringSmooth); //camera orientation
-                if (transform.rotation == cameraRotation)
-                    rotationRecovering = false;
-            }
-            else
-                transform.rotation = Quaternion.Euler(rotationX, rotationY, 0);
-
-            altPreviouslyPushed = altPushed;
+            rotationX = lastRotationX;
+            rotationY = FollowedPlayer.transform.localRotation.eulerAngles.y;
+            rotationRecovering = true;
         }
-    }
 
-    public void IsInMenu(bool inMenu)
-    {
-        this.inMenu = inMenu;
-        Cursor.visible = inMenu;
-        if (inMenu)
-            Cursor.lockState = CursorLockMode.None;
+        //mouse & stick inputs supported
+        float FinalInputX = Input.GetAxis("RightStickHorizontal") + Input.GetAxis("Mouse X");
+		float FinalInputZ = Input.GetAxis("RightStickVertical") + Input.GetAxis("Mouse Y");
+
+		//charge rotations
+		rotationY += (FinalInputX * CameraHorizontalSensitivity) * PlayerPrefs.GetFloat("Y_axis_sensitivity") * Time.deltaTime;
+		rotationX += (FinalInputZ * CameraVerticalSensitivity) * PlayerPrefs.GetFloat("X_axis_sensitivity") * Time.deltaTime;
+
+		rotationX = Mathf.Clamp(rotationX, MinClampAngle, MaxClampAngle);
+
+        if (!altPushed)
+        {
+            lastRotationX = rotationX;
+            FollowedPlayer.transform.rotation = Quaternion.Euler(0, rotationY, 0); //player orientation
+        }
+
+        Quaternion cameraRotation = Quaternion.Euler(rotationX, rotationY, 0);
+
+        if (rotationRecovering)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, cameraRotation, Time.deltaTime * RotationRecoveringSmooth); //camera orientation
+            if (transform.rotation == cameraRotation)
+                rotationRecovering = false;
+        }
         else
-            Cursor.lockState = CursorLockMode.Locked;
+            transform.rotation = Quaternion.Euler(rotationX, rotationY, 0);
+
+        altPreviouslyPushed = altPushed;
     }
 }
