652aa3f75b7c6900798147125fe9a46f4191a3c1
Blatantbeast <35450175+Blatantbeast@users.noreply.github.com>
2018-12-06 16:13:12 +0100

    Fabien

---
+++ RageQuit/Assets/Fabien/map 3/script levl3/nn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nn : MonoBehaviour {
+    private Animator Anim;
+	// Use this for initialization
+	void Start () {
+        Anim = GameObject.Find("p").GetComponent<Animator>();
+		
+	}
+    void OnTriggerEnter()
+    {
+        Anim.SetBool("isbool", true);
+    }
+    void OnTriggerExit()
+    {
+        Anim.SetBool("isbool", false);
+    }
+
+}
---
+++ RageQuit/Assets/Fabien/map 3/script levl3/rotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour {
+    public float speedx = 0;
+    public float speedy = 0;
+    public float speedz = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(speedx, speedy, speedz); 
+		
+	}
+}
